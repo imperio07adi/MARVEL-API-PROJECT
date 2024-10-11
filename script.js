fdd450c49b0f9b5dd2c238b2d836f3c8d9ef63bf
imperio07adi <adi310kokashe@gmail.com>
2024-10-11 15:01:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const publicKey = 'ac925d77d596c2b8f5f04e3faa04a48b';
+const privateKey = 'e86f17babd7d8b9cb8718fd957a0813bd51a2475';
+
+
+function generateMD5Hash(ts, privateKey, publicKey) {
+    return CryptoJS.MD5(ts + privateKey + publicKey).toString();
+}
+
+
+function fetchCharacterData(characterName) {
+    const ts = new Date().getTime();  
+    const hash = generateMD5Hash(ts, privateKey, publicKey);  
+    const url = `http://gateway.marvel.com/v1/public/characters?name=${characterName}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCharacterInfo(data))
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+
+function displayCharacterInfo(data) {
+    const characterInfoDiv = document.getElementById('characterInfo');
+    characterInfoDiv.innerHTML = '';
+
+    if (data && data.data && data.data.results.length > 0) {
+        const character = data.data.results[0];
+        characterInfoDiv.innerHTML = `
+            <h2>${character.name}</h2>
+            <p>${character.description || 'No description available.'}</p>
+            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}">
+        `;
+    } else {
+        characterInfoDiv.innerHTML = '<p>No character found.</p>';
+    }
+}
+
+
+document.getElementById('searchButton').addEventListener('click', function () {
+    const characterName = document.getElementById('characterName').value;
+    if (characterName) {
+        fetchCharacterData(characterName);
+    } else {
+        alert('Please enter a character name!');
+    }
+});
