74dcb82d7c0161bbb48c61b57d70e463c1104df7
Aditya Kokashe <adi310kokashe@gmail.com>
2025-02-12 22:44:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,46 @@
-body {
-    font-family: Arial, sans-serif;
-    background-color: #984447;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 80vh;
-    background-image:url("https://wallpapercat.com/w/full/e/c/6/950708-3840x2160-desktop-4k-mcu-background-photo.jpg");
-}
+const publicKey = 'ac925d77d596c2b8f5f04e3faa04a48b';
+const privateKey = 'e86f17babd7d8b9cb8718fd957a0813bd51a2475';
 
-.container {
-    text-align: center;
-    background-color: #468C98;
-    padding: 80px;
-    border-radius: 20px;
-    font-size: 40px;
-    margin-top: 20%;
-    margin-bottom: 10%;
-    margin-left: 10%;
-    margin-right: 10%;;
 
-}
-
-input {
-    padding: 20px;
-    font-size: 40px;
-    text-align: center;
-    width: 90%;
-    border: none;
-    border-radius: 20px;
-
-}
-
-button {
-    margin:50px;
-    padding: 10px 40px;
-    font-size: 30px;
-    background-color: #476C9B;
-    cursor: pointer;
-    border: none;
-    border-radius: 5px;
-
-}
-
-button:hover {
-    background-color: yellow;
-
+function generateMD5Hash(ts, privateKey, publicKey) {
+    return CryptoJS.MD5(ts + privateKey + publicKey).toString();
 }
 
 
+function fetchCharacterData(characterName) {
+    const ts = new Date().getTime();  
+    const hash = generateMD5Hash(ts, privateKey, publicKey);  
+    const url = `http://gateway.marvel.com/v1/public/characters?name=${characterName}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCharacterInfo(data))
+        .catch(error => console.error('Error fetching data:', error));
+}
 
 
+function displayCharacterInfo(data) {
+    const characterInfoDiv = document.getElementById('characterInfo');
+    characterInfoDiv.innerHTML = '';
+
+    if (data && data.data && data.data.results.length > 0) {
+        const character = data.data.results[0];
+        characterInfoDiv.innerHTML = `
+            <h3>${character.name}</h3>
+            <p>${character.description || 'No description available.'}</p>
+            <img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" height="20%" width="20%">
+        `;
+    } else {
+        characterInfoDiv.innerHTML = '<p>No character found.</p>';
+    }
+}
 
 
+document.getElementById('searchButton').addEventListener('click', function () {
+    const characterName = document.getElementById('characterName').value;
+    if (characterName) {
+        fetchCharacterData(characterName);
+    } else {
+        alert('Please enter a character name!');
+    }
+});
